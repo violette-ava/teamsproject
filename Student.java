d20671698c5060a686e0315fd1e91d4ae70d0172
Ava Violette <avakviolette@yahoo.com>
2022-10-03 00:23:59 -0500

    Outline for Teacher and Course input

--- Student.java
+++ Student.java
@@ -1,5 +1,6 @@
 import java.io.Serializable;
 
 public class Student implements Serializable {
+    Student student1 = new Student() //HOW DO I USE THE INFO FROM THE OTHER CLASS TO MAKE THIS OBJECT 
     
 }
--- TEAMS.java
+++ TEAMS.java
@@ -46,6 +46,10 @@ public class TEAMS{
                 System.out.println("1) Add Students "); 
                 System.out.println("2) Add Teacher");
                 System.out.println("3) Add Course");
+                System.out.println("4) Edit Students");
+                System.out.println("5) List All Teachers");
+                System.out.println("6) List All Courses");
+                System.out.println("7) Search for a Student");
                 System.out.println("0) Previous Page");
                 System.out.println();//EMPTY LINE
                 //get the choice
@@ -65,23 +69,48 @@ public class TEAMS{
                     System.out.println("Add a Student");
                     System.out.println("Enter New Student Name: ");
                     Scanner fetchstudent = new Scanner(System.in);
+                    // HOW DO I CREATE AN OBJECT USING USER INPUT
                     System.out.println("Enter grade level: ");
                     Scanner fetchgrade = new Scanner(System.in);
+                    Student student1 = new Student()
+                    //HOW DO I RETURN TO MAIN MENU?
 
                 }
             
                 public static void optionTwo(){
-                    System.out.println("Add a Teacher");
-                    
+                    System.out.println("Add a Teacher:");
+                    System.out.println("Enter Teacher Name: ");
+                    Scanner fetchteacher = new Scanner(System.in);
+                    System.out.println("Enter Years of Experience: ");
+                    Scanner fetchexperience = new Scanner(System.in);
+
                 }
             
                 public static void optionThree(){
                     int choice = 1;
                     while(choice > 0){
                         //Print the Menu
-                        System.out.print("#### OPTION 3 Menu ####");
-                        System.out.println("1) Sub Option 1"); 
-                        System.out.println("2) Sub Option 2");
+                        System.out.print("Add a Course");
+                        System.out.println("Enter Subject: "); 
+                        Scanner fetchsubject = new Scanner(System.in);
+                        System.out.println("Pick a Teacher"){
+                            import java.util.ArrayList; 
+                            public class ListsWithStrings {
+                            public static void main(String[] args) {
+                             ArrayList<String> list = new ArrayList<String>();  //HOW DO I ADD TEACHERS SPECIFIC TO A COURSE?
+                                
+                             list.add("James");
+                            list.add("Steve");
+                             list.add("Amanda");
+                             list.add("Rachael");
+        
+                         System.out.println(list);
+                         list.size(); 
+                            list.get(); 
+                            }
+                        }
+                    
+
                         System.out.println("3) Sub Option 3");
                         System.out.println("0) Exit");
                         System.out.println();//EMPTY LINE
